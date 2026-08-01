a9538e49411763377d58b0fe7596653159fdf95d
annaskhan-dev <kanas1431@gmail.com>
2026-08-02 01:02:49 +0500

    Initial commit for RCHY portfolio

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: './', // This ensures assets load with relative paths
   plugins: [react()],
 })
