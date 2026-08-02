173af28d077a2461ae510a14ce2c547055738120
annaskhan-dev <kanas1431@gmail.com>
2026-08-02 11:17:30 +0500

    Fix hero section alignment and class naming

--- src/components/herosection.jsx
+++ src/components/herosection.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import style from "./herosection.module.css";
-import herofooter from "../assets/rbridge.svg"; 
+import heroimage from "../assets/heroimage.svg";
+import star1 from "../assets/Star 1.svg";
+import herofooter from "../assets/rbridge.svg";
 import bridge from "../assets/logorbridge.svg";
 import R from "../assets/BR.png";
 import C from "../assets/BC.png";
@@ -10,36 +12,40 @@ import Y from "../assets/BY.png";
 const HeroSection = () => {
   return (
     <section className={style.rchy_hero_section}>
-      {/* Navigation */}
-      <nav className={style.rchy_nav_bar}>
-        <p className={style.rchy_title}>Mind The Gap</p>
-        <div className={style.rchy_logo_container}>
-          <img src={bridge} alt="Bridge Logo" className={style.rchy_bridge} />
-          <div className={style.rchy_letters_wrapper}>
-            <img src={R} alt="R" className={style.rchy_logo_letter} />
-            <img src={C} alt="C" className={style.rchy_logo_letter} />
-            <img src={H} alt="H" className={style.rchy_logo_letter} />
-            <img src={Y} alt="Y" className={style.rchy_logo_letter} />
-          </div>
-        </div>
-      </nav>
-
-      {/* Main Hero Content (Headline + Footer block) */}
+      {/* Wrapper added to constrain width */}
       <div className={style.rchy_main_wrapper}>
         
-        <h1 className={style.rchy_headline}>
-          We Make Clicks<br />
-          Feel Inevitable.
-        </h1>
+        <nav className={style.rchy_nav_bar}>
+          <p className={style.rchy_title}>Mind The Gap</p>
+          
+          <div className={style.rchy_logo_container}>
+            <img src={bridge} alt="Bridge Logo" className={style.rchy_bridge} />
+            <div className={style.rchy_letters_wrapper}>
+              <img src={R} alt="R" className={style.rchy_logo_letter} />
+              <img src={C} alt="C" className={style.rchy_logo_letter} />
+              <img src={H} alt="H" className={style.rchy_logo_letter} />
+              <img src={Y} alt="Y" className={style.rchy_logo_letter} />
+            </div>
+          </div>
+        </nav>
 
-        <div className={style.rchy_footer_wrapper}>
-          <img src={herofooter} alt="Hero Footer" className={style.rchy_footer_image} />
-          <p className={style.rchy_footer_text}>
-            We Design Logos. We Build Websites. We Run <br />
-            Campaigns. Everyone Says That. We'd rather <br />
-            Understand What Your Business Needs, Remove <br />
-            what's Getting In The Way, And Create Work That <br />
-            Earns Attention Instead Of Demanding It.
+        <div className={style.rchy_hero_content}>
+          <img src={heroimage} alt="Hero" className={style.rchy_hero_image} />
+          <img src={star1} alt="Star Element" className={style.rchy_star_element} />
+        </div>
+
+        <div className={style.rchy_hero_footer}>
+          <img
+            src={herofooter}
+            alt="Hero Footer"
+            className={style.rchy_hero_footer_image}
+          />
+          <p className={style.rchy_hero_footer_text}>
+            We Design Logos. We Build Websites. We Run
+            <br /> Campaigns. Everyone Says That. We'd rather
+            <br /> Understand What Your Business Needs, Remove
+            <br /> what's Getting In The Way, And Create Work That
+            <br /> Earns Attention Instead Of Demanding It.
           </p>
         </div>
       </div>
